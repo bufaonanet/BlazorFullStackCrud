8dda9d3fc8f2180ad87a72bb1df6d7c4022e2b3c
bufaonanet <douglas.bufaonanet@outlook.com>
2023-01-10 10:24:26 -0300

    Add project files.

---
+++ BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,126 @@
+﻿using BlazorFullStackCrud.Server.Data;
+using BlazorFullStackCrud.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorFullStackCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<Comic> mockComics = new()
+        {
+            new Comic{Id = 1, Name="Marvel"},
+            new Comic{Id = 2, Name="DC"}
+        };
+
+        public static List<SuperHero> mockHeroes = new()
+        {
+            new SuperHero
+            {
+                Id = 1,
+                FirstName = "Perter",
+                LastName = "Parker",
+                HeroName ="Spiderman",
+                Comic = mockComics[0],
+                ComicId = 1,
+            },
+            new SuperHero
+            {
+                Id = 2,
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                HeroName ="Batman",
+                Comic = mockComics[1],
+                ComicId = 2,
+            }
+        };
+
+
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("comics")]
+        public async Task<ActionResult<List<Comic>>> GetComics()
+        {
+            var comics = await _context.Comics.ToListAsync();
+            return Ok(comics);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            var heroes = await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+            return Ok(heroes);
+        }        
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var hero = await _context.SuperHeroes
+                .Include(h => h.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
+            if (hero == null)
+            {
+                return NotFound("Sorry, no hero here. :/");
+            }
+            return Ok(hero);
+        }            
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            hero.Comic = null;
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero, int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(sh => sh.Id == id);
+            if (dbHero == null)
+                return NotFound("Sorry, but no hero for you. :/");
+
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.HeroName = hero.HeroName;
+            dbHero.ComicId = hero.ComicId;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(sh => sh.Id == id);
+            if (dbHero == null)
+                return NotFound("Sorry, but no hero for you. :/");
+
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        private async Task<List<SuperHero>> GetDbHeroes()
+        {
+            return await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+        }
+
+
+    }
+}
